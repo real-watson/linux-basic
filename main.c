411512628af666030986caed6678b59d991ce2a4
root <watson@example.com>
2020-03-21 23:16:14 +0800

    fix added the reverse of data 	modified:   main.c

--- main.c
+++ main.c
@@ -8,7 +8,11 @@ typedef unsigned int uint32;
 					 (((uint32)(be) & 0x0000ff00) << 8)  |\
 					 (((uint32)(be) & 0x000000ff) << 24)  \
 					 )
-
+/*0x12345678 -> 0x5678 1234 reverse*/
+#define BASE_BE_TO_LE(be) ( \
+					(((uint32)(be) & 0xffff0000) >> 16) |\
+					(((uint32)(be) & 0x0000ffff) << 16) \
+					) 
 /*
 *name:query mode from system
 *input param: null
