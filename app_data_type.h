4a99e4510c6542a3db82df90775ff83439da75fd
real-watson <watsonwaitingme@gmail.com>
2020-05-04 13:32:42 +0800

    fix 注释const代码
    	modified:   app_data_type.h

--- app_data_type.h
+++ app_data_type.h
@@ -83,11 +83,11 @@ int size_space_full()
 }
 
 /*const*/
-const int a;/*a是一个常数不可修改*/
-int const a;/*同上*/
-const int *a;/*一个指向cosnt对象指针，不能通过这个指针修改这个值*/
-int * const a;/*指针无法修改*/
-int const * a const;/*都无法修改*/
+//const int a;/*a是一个常数不可修改*/
+//int const a;/*同上*/
+//const int *a;/*一个指向cosnt对象指针，不能通过这个指针修改这个值*/
+//int * const a;/*指针无法修改*/
+//int const * a const;/*都无法修改*/
 
 #define _APP_DATA_TYPE_
 #endif 
