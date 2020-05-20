4a3d6ecb4b29995ba17518c9364bbe5c360dc375
root <watson@example.com>
2020-05-20 14:47:45 +0800

    fix add swap function. 	增加交换函数，通过传入地址，交换两个数。
    	modified:   app_self_alhm.c

--- app_self_alhm.c
+++ app_self_alhm.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void swap_number(int *x, int *y)
+{
+	int it;/*临时量作为交换*/
+	it = *x;
+	*x = *y;
+	*y = it;
+}
+
 void bubble_sort (int *a,int n)
 {
 	int i,j,temp;
@@ -7,9 +15,7 @@ void bubble_sort (int *a,int n)
 	for(i=0;i<n;i++){
 		for(j=1;j<n-i;j++){
 			if(a[j]<a[j-1]){
-				temp=a[j];
-				a[j] = a[j-1];
-				a[j-1] = temp;
+				swap_number(&a[j],&a[j-1]);
 			}
 		}//end j for
 	}//end i for
