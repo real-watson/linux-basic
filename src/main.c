aab3770b624d079c9a115ced587c6fdfa331ec1f
real-watson <watsonwaitingme@gmail.com>
2020-05-24 22:35:36 +0800

    fix moving some files
    	renamed:    test.txt -> file/test.txt
    	renamed:    LICENSE -> licence/LICENSE
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@ int main(void)
 	printf("The size of them are %lu for struct, %lu for union in same data \n", SIZEOFSTRUCT, SIZEOFUNION); printf("The fixed structs is %lu\n",SIZEOFALL);
 	int res1,res2; int x = 9; int y = 29; char *string = NULL;
 	char *str = NULL;
-	res1 = file_size_withf("../test.txt"); 
-	res2 = file_size_withoutf("../test.txt"); 
+	res1 = file_size_withf("../file/test.txt"); 
+	res2 = file_size_withoutf("../file/test.txt"); 
 	printf("The size of file is %d,and %d\n",res1,res2);
 	printf("The biggest one is %d %d\n",MAX_2(1,2),MAX_3(1,2,3));
 	self_swap_number(&x,&y);
